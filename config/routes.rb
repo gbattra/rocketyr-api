4137eda015a24260b260631d3d170df612b76c2e
G. Broderick Attra <greg.attra@gmail.com>
2018-07-11 22:04:48 -0400

    moved resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,23 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
-  resources :tracks
-  resources :track_ranks
-  resources :genres
-  resources :albums
-  resources :author_profiles
-  resources :locations
-  resources :users
-  resources :feeds
-  resources :icons
-  resources :playlists
-  resources :feed_tracks
-  resources :playlist_tracks
-  resources :profile_tracks
-  resources :liked_tracks
-  resources :badges
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-        end
+      resources :tracks
+      resources :track_ranks
+      resources :genres
+      resources :albums
+      resources :author_profiles
+      resources :locations
+      resources :users
+      resources :feeds
+      resources :icons
+      resources :playlists
+      resources :feed_tracks
+      resources :playlist_tracks
+      resources :profile_tracks
+      resources :liked_tracks
+      resources :badges
+    end
   end
 end
